9f41a89458eb9b1d42c635fc0306024f1be606e7
Julia Szczuczko <julia@adcon.pl>
2024-06-27 15:47:41 +0200

    base project setup

---
+++ MosaicWars/Source/MosaicWars/MosaicWars.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MosaicWars.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MosaicWars, "MosaicWars" );
+ 
